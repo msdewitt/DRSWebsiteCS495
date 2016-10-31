f7d7c967ecb23a3027888596a2ef077541dddc85
Matheu DeWitt <msdewitt@bsu.edu>
2016-10-31 13:18:47 -0400

    Removed merge error conflicts.

--- app/login/login.module.js
+++ app/login/login.module.js
@@ -1,11 +1,4 @@
 (function(){
-<<<<<<< HEAD
   'use strict';
   angular.module('drsApp.login',[])
-=======
-  'use strict'
-
-  var app =
-  angular.module('drsApp.login', []);
->>>>>>> 46258be29019c430dd816052eeb0f7cb13b17769
 })()
