e5c5302bf75ded082f9438dc6c785949b271fd45
Matheu DeWitt <msdewitt@bsu.edu>
2016-11-08 14:29:12 -0500

    Add firebaseData.service.js and a core module.

---
+++ app/core/core.module.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('drsApp.core',[]);
+    //.constant('FIREBASE_URL',... );
+})()
---
+++ app/core/firebaseData.service.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('drsApp.core')
+    .factory('firebaseDataService', firebaseDataService);
+
+  function firebaseDataService() {
+    var root = firebase.database().ref();
+
+    var service = {
+      root: root,
+      // texts: root.child('texts'),
+      // mutants: root.child('mutants'),
+       users: root.child('users'),
+       emails: root.child('users'),
+    };
+
+    return service;
+  }
+})();
