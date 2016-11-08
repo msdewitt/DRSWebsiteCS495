5603ae073cc70f5479985a6295b30189e4ee0d4a
Matheu DeWitt <msdewitt@bsu.edu>
2016-11-08 14:35:30 -0500

    Remove firebase config from git ignore.

---
+++ app/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAjjzDu24nnJNEmcvUAl1p5xmmkoV4JwG4",
+  authDomain: "drsapptest-dc038.firebaseapp.com",
+  databaseURL: "https://drsapptest-dc038.firebaseio.com",
+  storageBucket: "drsapptest-dc038.appspot.com",
+  messagingSenderId: "386548238481"
+};
+firebase.initializeApp(config);
