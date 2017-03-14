d2d76b22634de1563aa6d753bcbc371e5a6e71aa
Matheu DeWitt <msdewitt@bsu.edu>
2017-03-14 14:23:51 -0400

    Add timetable.js for scheduling.

--- app/schedule/schedule.controller.js
+++ app/schedule/schedule.controller.js
@@ -8,6 +8,21 @@
   ScheduleController.$inject = ['$state', 'authService'];
 
   function ScheduleController($state, authService) {
-    var vm = this;
+    var timetable = new Timetable();
+    timetable.setScope(9,17);
+    timetable.addLocations(['Silent Disco', 'Nile', 'Len Room', 'Maas Room']);
+    timetable.addEvent('Frankadelic', 'Nile', new Date(2015,7,17,10,45), new Date(2015,7,17,12,30));
+    var options = {
+  url: '#',
+  class: 'vip',
+  data: {
+    id: 4,
+    ticketType: 'VIP'
+  }
+}
+timetable.addEvent('Jam Session', 'Nile', new Date(2015,7,17,21,15), new Date(2015,7,17,23,30), options);
 
+    var renderer = new Timetable.Renderer(timetable);
+    renderer.draw('.timetable');
+}
 });
